7496a0c97db2b5585e60080e48c8e6cf39e92365
User <User@LAB3-3>
2023-11-29 09:13:02 +0200

    Update DataBase + manager,subjects,questions.

--- Trivia_Stage1/SQL script/TriviaDatabase.sql
+++ Trivia_Stage1/SQL script/TriviaDatabase.sql
@@ -1,8 +1,26 @@
-CREATE DATABASE["Trivia"];
+CREATE DATABASE[Trivia];
 
-use ["Trivia"]
+use [Trivia]
 
-CREATE TABLE["Players"]
+CREATE TABLE [Subjects]
+(
+[subjectCode] int Identity(1,1) PRIMARY KEY not null,
+[name] nvarchar(250) not null
+);
+
+CREATE TABLE [Status]
+(
+[statusCode] int Identity(1,1) PRIMARY KEY not null,
+[name] nvarchar(250)  not null
+);
+
+CREATE TABLE [Levels]
+(
+[levelCode] int Identity(1,1) PRIMARY KEY not null,
+[name] nvarchar(250) not null
+);
+
+CREATE TABLE[Players]
 (
 [playerMail] nvarchar(250) PRIMARY KEY not null,
 [name] nvarchar(250) not null,
@@ -10,14 +28,43 @@ CREATE TABLE["Players"]
 [points] int not null
 );
 
-CREATE TABLE ["Levels"]
-(
-[levelCode] int Identity(1,1) PRIMARY KEY not null,
-[name] nvarchar(250) not null
-);
 
-CREATE TABLE["Questions"]
+CREATE TABLE[Questions]
 (
 [questionId] int PRIMARY KEY not null,
-[subjectCode] int not null,
+[subjectCode] int not null FOREIGN KEY References Subjects(subjectCode),
+[text] nvarchar(250) not null,
+[correctAns] nvarchar(250) not null,
+[wrongAns1] nvarchar(250) not null,
+[wrongAns2] nvarchar(250) not null,
+[wrongAns3] nvarchar(250) not null,
+[createdBy] nvarchar(250)  not null FOREIGN KEY References Players(playerMail),
+[statusCode] int not null FOREIGN KEY References Status(statusCode)
 );
+
+INSERT INTO [Levels] (name) values('rookie');
+INSERT INTO [Levels] (name) values('master');
+INSERT INTO [Levels] (name) values('manager');
+
+INSERT INTO [Players] (playerMail, name,levelCode, points ) values('ronen@gmail.com', 'Ronen',3,1000);
+
+INSERT INTO [Subjects] (name) values('sports');
+INSERT INTO [Subjects] (name) values('politics');
+INSERT INTO [Subjects] (name) values('history');
+INSERT INTO [Subjects] (name) values('science');
+INSERT INTO [Subjects] (name) values('ramonHighSchool');
+
+INSERT INTO [Status] (name) values('approved');
+INSERT INTO [Status] (name) values('notApproved');
+INSERT INTO [Status] (name) values('waiting');
+
+INSERT INTO [Questions] (questionId, subjectCode, text, correctAns, wrongAns1, wrongAns2, wrongAns3, createdBy, statusCode) values(1, 1, 'How many km are in a marathon?','42.4','3.2','100','1', '',1);
+
+INSERT INTO [Questions] (questionId, subjectCode, text, correctAns, wrongAns1, wrongAns2, wrongAns3, createdBy, statusCode) values(2,2,'Who is the USA president?', 'Joe Baiden', 'Barak Obama','Donald Trump','Bibi', '',1);
+
+INSERT INTO [Questions] (questionId, subjectCode, text, correctAns, wrongAns1, wrongAns2, wrongAns3, createdBy, statusCode) values(3,3,'When did WWI started?','1914','1932','1917','1915','',1);
+
+INSERT INTO [Questions] (questionId, subjectCode, text, correctAns, wrongAns1, wrongAns2, wrongAns3, createdBy, statusCode) values(4,4, 'What is the atomic number of oxygen?','8','2','7','12','',1);
+
+INSERT INTO [Questions] (questionId, subjectCode, text, correctAns, wrongAns1, wrongAns2, wrongAns3, createdBy, statusCode) values(5,5, 'What is the name og the principle in Ramon High School?', 'Hana', 'Leah', 'Rebecca','Moshe','',1);
+
